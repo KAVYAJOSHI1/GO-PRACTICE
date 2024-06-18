b800e53e47b050d9aada9a7a87b5c1cece60762a
KAVYAJOSHI1 <kavyajoshi152005@gmail.com>
2024-06-18 16:59:10 +0530

    commit

--- SUM.go
+++ SUM.go
@@ -1,15 +1,14 @@
 //PROGRAM TO ADD TO NUMBERS
 package main
 import "fmt"
-func main()
-{
-	var a int
-	var b int
-	var c int
-	fmt.Print("ENTER THE FIRST NUMBER")
-	fmt.Scan(&a)
-	fmt.Print("ENTER THE SECOND NUMBER")
-	fmt.Scan(&b)
-	c=a+b
-	fmt.Print("THE SUM OF ",a," AND ",b," IS ",c)
+func main(){
+var a int
+var b int
+var c int
+fmt.Print("ENTER THE FIRST NUMBER")
+fmt.Scan(&a)
+fmt.Print("ENTER THE SECOND NUMBER")
+fmt.Scan(&b)
+c=a+b
+fmt.Print("THE SUM OF ",a," AND ",b," IS ",c)
 }
