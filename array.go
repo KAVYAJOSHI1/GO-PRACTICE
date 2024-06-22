26fb335355fc34dd9fd645539525ebb508d05aee
KAVYAJOSHI1 <kavyajoshi152005@gmail.com>
2024-06-22 16:44:01 +0530

    commit

---
+++ array.go
@@ -0,0 +1,11 @@
+package main
+import "fmt"
+func main(){
+var arr[5] int={10,20,30,40,50}
+arr[0]=10
+arr[1]=20
+arr[2]=30
+arr[3]=40
+arr[4]=50
+fmt.Print(arr)
+}
---
+++ nogreaterthan5.go
@@ -0,0 +1,11 @@
+//TO CHECK WEATHER THE NUMBER IS GREATER THAN 5 OR NOT
+package main
+import "fmt"
+func main(){
+	var n int;
+	fmt.Print("ENTER THE NUMBER\n")
+	fmt.Scan(&n)
+	if(n>5){
+		fmt.Print("THE NUMBER IS GREATER THAN 5\n")
+	}
+}
